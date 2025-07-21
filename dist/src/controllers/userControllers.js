13a06f378082de543bb162d9e7f1c5b2e4ab43a3
Nic Babineaux <76629649+NicBab@users.noreply.github.com>
2025-07-21 18:40:47 -0500

    debugging front end renderings form api not working but can GET in postman

--- dist/src/controllers/userControllers.js
+++ dist/src/controllers/userControllers.js
@@ -1,5 +1,47 @@
 "use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
 Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = require("express");
-const router = (0, express_1.Router)();
-exports.default = router;
+exports.deleteUser = exports.getUsers = void 0;
+const client_1 = require("@prisma/client");
+const prisma = new client_1.PrismaClient();
+const getUsers = (req, res) => __awaiter(void 0, void 0, void 0, function* () {
+    var _a;
+    try {
+        const search = (_a = req.query.search) === null || _a === void 0 ? void 0 : _a.toString();
+        const users = yield prisma.users.findMany({
+            where: search
+                ? {
+                    OR: [
+                        { name: { contains: search, mode: "insensitive" } },
+                    ],
+                }
+                : undefined, // return all if no search
+        });
+        res.json(users);
+    }
+    catch (error) {
+        res.status(500).json({ message: "Error retrieving users" });
+    }
+});
+exports.getUsers = getUsers;
+const deleteUser = (req, res) => __awaiter(void 0, void 0, void 0, function* () {
+    try {
+        const { id } = req.params;
+        const deletedUser = yield prisma.users.delete({
+            where: { userId: id },
+        });
+        res.status(200).json({ message: "Product deleted", deletedUser });
+    }
+    catch (error) {
+        res.status(500).json({ message: "Error deleting product" });
+    }
+});
+exports.deleteUser = deleteUser;
--- src/controllers/userControllers.ts
+++ src/controllers/userControllers.ts
@@ -1,5 +1,37 @@
-import { Router } from "express";
+import { Request, Response } from "express";
+import { PrismaClient } from "@prisma/client";
 
-const router = Router();
+const prisma = new PrismaClient();
 
-export default router;
+export const getUsers = async (req: Request, res: Response): Promise<void> => {
+  try {
+     const search = req.query.search?.toString();
+    const users = await prisma.users.findMany({
+      where: search
+        ? {
+            OR: [
+              { name: { contains: search, mode: "insensitive" } },
+            ],
+          }
+        : undefined, // return all if no search
+    });
+    res.json(users);
+  } catch (error) {
+    res.status(500).json({ message: "Error retrieving users" });
+  }
+};
+
+export const deleteUser = async (
+  req: Request,
+  res: Response
+): Promise<void> => {
+  try {
+    const { id } = req.params;
+    const deletedUser = await prisma.users.delete({
+      where: { userId: id },
+    });
+    res.status(200).json({ message: "Product deleted", deletedUser });
+  } catch (error) {
+    res.status(500).json({ message: "Error deleting product" });
+  }
+};
